e5c513e67927ff13563b5df02d837cc29f16dff5
Carlo D'Urso <carlodurso@gmail.com>
2019-10-15 20:11:46 +0100

    refining scheduler

--- routes.js
+++ routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const router = express.Router();
-const Forecast = require('./models/forecast');
+const Forecast = require('forecast.js');
 const https = require('https');
 var js2xmlparser = require("js2xmlparser");
 
