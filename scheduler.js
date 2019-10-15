8bd8968f468cea9570d6a221129b9be6b5ca146c
Carlo D'Urso <carlodurso@gmail.com>
2019-10-15 20:34:47 +0100

    refining scheduler

--- scheduler.js
+++ scheduler.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 const mongoose = require("mongoose");
 const Forecast = require('./forecast');
 const https = require('https');
@@ -8,11 +8,13 @@ var url = 'https://api.darksky.net/forecast/0c9754ea8065c38a4de8c8714434aaf3/51.
 var mongourl = process.env.MONGODB_URI || 'mongodb://localhost/forecast';
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect(mongourl)
+mongoose.connect(mongourl, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, res) {
+  if (err) console.log ('ERROR connecting to: ' + mongourl + '. ' + err);
+})
 
-router.get('/', function (request, response) {
+// router.get('/', function (request, response) {
 
-    let req = https.get(url, function(res) {
+    https.get(url, function(res) {
       let data = '',
         json_data;
 
@@ -24,7 +26,7 @@ router.get('/', function (request, response) {
       
 
         var json = json_data.daily.data;
-        response.status(200).json(json);
+            console.log(json);
 
           for(var i = 0; i < json.length; i++) {
             var obj = json[i];
@@ -45,5 +47,5 @@ router.get('/', function (request, response) {
     req.on('error', function(e) {
         console.log(e.message);
     });
-  });
+  // });
 // process.exit();
